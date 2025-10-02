62a151fb2b06efaf8e94de2409c6cc7cc64419f7
Matheus-Vecchi <matheusvecchi02@gmail.com>
2025-10-02 13:22:49 -0300

    feat(Header): cria e estiliza componente Header

---
+++ access-control-cp/src/components/Header/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return(
+        <header className="h-[8vh] sm:h-[10vh] bg-[#00532E] shadow-xl flex items-center justify-center">
+            <img className="w-[140px] sm:w-[160px] mt-[90px]" src="/palmeiras-logo.png" alt="Logo do Palmeiras"/>
+        </header>
+    );
+}
