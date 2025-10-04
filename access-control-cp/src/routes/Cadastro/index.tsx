1fcef679afc80826078e01410c8a9ceddae48092
Matheus-Vecchi <matheusvecchi02@gmail.com>
2025-10-04 19:56:42 -0300

    style(Cadastro): estiliza rota Cadastro

--- access-control-cp/src/routes/Cadastro/index.tsx
+++ access-control-cp/src/routes/Cadastro/index.tsx
@@ -1,7 +1,7 @@
 export default function() {
     return(
-        <main>
-            <h1>Cadastro</h1>
+        <main className="text-center mt-[12vh]">
+            <h1 className="text-[30px] sm:text-[40px]">Cadastro</h1>
         </main>
     );
 }
