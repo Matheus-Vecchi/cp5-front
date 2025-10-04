e045b0ab67316cf1f9c61d929cfb67289aae1f0f
Matheus-Vecchi <matheusvecchi02@gmail.com>
2025-10-04 19:47:07 -0300

    style(Login): estiliza rota Login

--- access-control-cp/src/routes/Login/index.tsx
+++ access-control-cp/src/routes/Login/index.tsx
@@ -1,5 +1,7 @@
 export default function Login() {
     return(
-        <h1>Login</h1>
+        <main className="text-center mt-[12vh]">
+            <h1 className="text-[30px] sm:text-[40px]">Login</h1>
+        </main>
     );
 }
