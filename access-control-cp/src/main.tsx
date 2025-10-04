0cc194369809775d3592f957b441dc45268b2a6d
Matheus-Vecchi <matheusvecchi02@gmail.com>
2025-10-04 19:34:22 -0300

    feat(main): cria createBrowserRouter com react-router-dom e adiciona path /login na main.tsx

--- access-control-cp/src/main.tsx
+++ access-control-cp/src/main.tsx
@@ -4,6 +4,21 @@ import './globals.css'
 
 
 import App from './App.tsx'
+import Login from './routes/Login/index.tsx'
+
+
+import Error from './routes/Error/index.tsx'
+
+import { createBrowserRouter } from 'react-router-dom'
+
+
+
+
+const router = createBrowserRouter([
+  {path: "/login", element: <App/>, errorElement: <Error/>, children: [
+    {path: "/login", element: <Login/>}
+  ]}
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
