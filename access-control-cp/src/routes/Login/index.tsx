e78528461e766f3c6d493aefe478c5b797106bac
Matheus-Vecchi <matheusvecchi02@gmail.com>
2025-10-04 20:51:22 -0300

    feat(Login): cria lógica de autenticação de usuário

--- access-control-cp/src/routes/Login/index.tsx
+++ access-control-cp/src/routes/Login/index.tsx
@@ -1,7 +1,39 @@
+import { useForm } from "react-hook-form";
+import type { TipoLogin } from "../../types/tipoLogin";
+import { useNavigate } from "react-router-dom";
+
+
 export default function Login() {
+    const navigate = useNavigate();
+    const { register, handleSubmit, formState: { errors } } = useForm<TipoLogin>();
+
+    const onSubmit = async (data: TipoLogin) => {
+        try {
+            const res = await fetch("http://localhost:3001/usuarios");
+            const usuarios = await res.json();
+
+            const user = usuarios.find(
+                (u: any) => u.nomeUsuario === data.nomeUsuario && u.email === data.email
+            );
+
+            if (user) {
+                sessionStorage.setItem("usuarioLogado", JSON.stringify(user));
+                navigate("/");
+            } 
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
     return(
         <main className="text-center mt-[12vh]">
             <h1 className="text-[30px] sm:text-[40px]">Login</h1>
+
+            <div>
+                <form onSubmit={handleSubmit(onSubmit)}>
+
+                </form>
+            </div>
         </main>
     );
 }
