47b47a0d9bc76046e2fc3912b30cd70229247f76
Matheus-Vecchi <matheusvecchi02@gmail.com>
2025-10-04 19:47:48 -0300

    fix: corrige erros na main e no Footer

--- access-control-cp/src/components/Footer/Footer.tsx
+++ access-control-cp/src/components/Footer/Footer.tsx
@@ -1,8 +1,8 @@
 export default function Footer() {
     return(
-        <footer className="text-center bg-[#00532E] p-8">
-            <p className="text-[18px] text-white">Palmeiras</p>
-            <p className="text-[18px] text-white">#MaiorCampeãoDoBrasil</p>
+        <footer className="text-[18px] sm:text-[22px] text-white text-center bg-[#00532E] p-8">
+            <p>Palmeiras</p>
+            <p>#MaiorCampeãoDoBrasil</p>
         </footer>
     );
 }
--- access-control-cp/src/main.tsx
+++ access-control-cp/src/main.tsx
@@ -14,7 +14,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 const router = createBrowserRouter([
   {path: "/", element: <App/>, errorElement: <Error/>, children: [
-    {path: "/login", element: <Login/>}
+    {path: "/", element: <Login/>}
   ]}
 ]);
 
