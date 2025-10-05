d6980651de3f3e93adcd2ada2d2010d2876dcf85
Matheus-Vecchi <matheusvecchi02@gmail.com>
2025-10-04 23:31:16 -0300

    style(Cadastro): cria e estiliza formulário de cadastro

--- access-control-cp/src/routes/Cadastro/index.tsx
+++ access-control-cp/src/routes/Cadastro/index.tsx
@@ -36,8 +36,38 @@ export default function Cadastro() {
     };
 
     return(
-        <main className="text-center mt-[12vh]">
-            <h1 className="text-[30px] sm:text-[40px]">Cadastro</h1>
+        <main className="min-h-screen flex flex-col items-center mt-[100px]">
+            <h1 className="text-[30px] sm:text-[40px] font-bold mb-6 text-[#00532E]">Cadastro</h1>
+
+            <form onSubmit={handleSubmit(onSubmit)} className="bg-[#00532E] rounded-lg p-2 w-full max-w-md flex flex-col space-y-4">
+                <fieldset className="w-full max-w-md p-4">
+                    <div className="flex flex-col">
+                        <label htmlFor="nome" className="text-white my-2 font-medium">Nome:</label>
+                        <input type="text" id="nome" placeholder="Nome completo" {...register("nome", { required: "Nome obrigatório" })} className="border border-white rounded-lg px-4 py-2 focus:outline-none focus:ring-2"/>
+                        {errors.nome && <span className="text-red-500">{errors.nome.message}</span>}
+                    </div>
+
+                    <div className="flex flex-col">
+                        <label htmlFor="nomeUsuario" className="text-white my-2 font-medium">Nome de usuário:</label>
+                        <input type="text" id="nomeUsuario" placeholder="Nome de usuário" {...register("nomeUsuario", { required: "Nome de usuário obrigatório" })} className="border border-white rounded-lg px-4 py-2 focus:outline-none focus:ring-2"/>
+                        {errors.nomeUsuario && <span className="text-red-500">{errors.nomeUsuario.message}</span>}
+                    </div>
+
+                    <div className="flex flex-col">
+                        <label htmlFor="email" className="text-white my-2 font-medium">Email:</label>
+                        <input type="text" id="email" placeholder="Email" {...register("email", { required: "Email obrigatório" })} className="border border-white rounded-lg px-4 py-2 focus:outline-none focus:ring-2"/>
+                        {errors.email && <span className="text-red-500">{errors.email.message}</span>}
+                    </div>
+
+                    <div className="flex">
+                        <button type="submit" className="font-medium mx-auto w-[130px] sm:w-[176px] mt-6 bg-white text-[#00532E] py-2 rounded-full">Cadastrar</button>
+                    </div>
+
+                    <div>
+                        <button type="button" onClick={() => navigate("/")} className="text-white mt-2">Já tem uma conta? Faça login!</button>
+                    </div>
+                </fieldset>
+            </form>
         </main>
     );
 }
