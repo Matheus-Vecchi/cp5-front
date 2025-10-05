07ba9cddbbe4e665cb395c5a79176a3b7d537891
Matheus-Vecchi <matheusvecchi02@gmail.com>
2025-10-04 22:53:19 -0300

    feat(Cadastro): cria lógica de cadastro

--- access-control-cp/src/routes/Cadastro/index.tsx
+++ access-control-cp/src/routes/Cadastro/index.tsx
@@ -1,4 +1,40 @@
-export default function() {
+import { useNavigate } from "react-router-dom";
+import type { TipoCadastro } from "../../types/tipoCadastro";
+import { useForm } from "react-hook-form";
+const API_URL = import.meta.env.VITE_API_URL_BASE;
+
+
+export default function Cadastro() {
+    const navigate = useNavigate();
+    const { register, handleSubmit, formState: { errors } } = useForm<TipoCadastro>();
+
+    const onSubmit = async (data: TipoCadastro) => {
+        try {
+            const res = await fetch(API_URL);
+            const usuarios = await res.json();
+
+            const duplicado = usuarios.find(
+                (u: any) => u.nomeUsuario === data.nomeUsuario || u.email === data.email
+            );
+
+            if (duplicado) {
+                alert("Nome de usuário ou email já cadastrado!");
+                return;
+            }
+
+            await fetch(API_URL, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(data)
+            });
+
+            alert("Cadastro realizado com sucesso!");
+            navigate("/login");
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
     return(
         <main className="text-center mt-[12vh]">
             <h1 className="text-[30px] sm:text-[40px]">Cadastro</h1>
